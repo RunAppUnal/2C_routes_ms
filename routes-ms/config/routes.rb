902c4eda1ae589eef314b8c41fb06d34e728d822
Miguel Villamil <miavillamilca@unal.edu.co>
2018-03-17 18:19:06 -0500

    adding routes to RESTful methods

--- routes-ms/config/routes.rb
+++ routes-ms/config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   resources :routes do
     member do
-      patch :add_user
+      patch :add_user #tested
+      patch :remove_user #tested
+      get :check_user #tested
+      get :users_in_route #tested
+      #get :my_routes
+    end
+
+    collection do
+      get :my_routes #tested
+      get :other_routes #tested
+      get :search_my_routes #tested
+      get :search_other_routes
     end
   end
 
