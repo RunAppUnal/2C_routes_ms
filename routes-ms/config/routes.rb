5c118d9803b9c248ebb4137b7347b9e5711e5430
Miguel Villamil <miavillamilca@unal.edu.co>
2018-03-19 18:31:24 -0500

    adding custom restful methods

--- routes-ms/config/routes.rb
+++ routes-ms/config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
   resources :routes do
     member do
-      patch :add_user #tested
-      patch :remove_user #tested
-      get :check_user #tested
-      get :users_in_route #tested
+      patch :add_user #tested Mutation        #GG
+      patch :remove_user #tested Mutation     #GG
+      get :check_user #tested Query           #charles
+      get :users_in_route #tested  Query      #not needed
       #get :my_routes
     end
 
     collection do
-      get :my_routes #tested
-      get :other_routes #tested
-      get :search_my_routes #tested
-      get :search_other_routes
+      get :my_routes #tested Query            #GG
+      get :other_routes #tested Query         #GG
+      get :search_my_routes #tested Query     #GG
+      get :search_other_routes #tested Query  #GG
     end
   end
 
